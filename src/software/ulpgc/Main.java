ca07ee116cf8df1dc671b181c6b2af1123e0130b
Alejandro <deolozaga2004@gmail.com>
2025-01-08 12:14:00 +0000

    Main class created.

--- src/software/ulpgc/Main.java
+++ src/software/ulpgc/Main.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Person("Alejandro", LocalDate.of(2004, 06, 11)));
+        System.out.println(new Person("Alejandro", LocalDate.of(2004, 6, 11)));
+        System.out.println(new Person("Lucas", LocalDate.of(1901, 1, 8)));
     }
 }
